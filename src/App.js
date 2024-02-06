b9c6b7c211d77e3200cbfd9a5017ef37f84096b8
nikhil shah <nikhil.shah@datamoulds.com>
2024-02-06 18:09:11 +0530

    NAV Calculator Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  Container,
+  Grid,
+  TextField,
+} from "@material-ui/core";
 
 function App() {
+  const NAV = 12.6897;
+
+  const [amount, setAmount] = useState();
+  const [unit, setUnit] = useState();
+  const [toggleAmountUnit, setToggleAmountUnit] = useState(false);
+
+  useEffect(() => setUnit(amount / NAV), [amount]);
+  useEffect(() => setAmount(unit * NAV), [unit]);
+
+  const reset = () => {
+    setAmount();
+    setUnit();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container maxWidth="md">
+      <Card
+        variant="outlined"
+        style={{ padding: "25px", marginTop: "35px", textAlign: "center" }}
+      >
+        <div style={{ margin: "20px 50px" }}>
+          <h3>NAV Calculator</h3>
+          <h4>
+            Scheme Name :{" "}
+            <u>Mutual Fund - Motilal Oswal Liquid Fund - Direct Growth</u>
+          </h4>
+
+          <h5>Net Asset Value - {NAV}</h5>
+        </div>
+        <br />
+        <Grid
+          container
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          style={{ minHeight: "30vh" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <ButtonGroup
+            variant="outlined"
+            aria-label="outlined primary button group"
+          >
+            <Button
+              color={toggleAmountUnit ? "secondary" : ""}
+              onClick={() => {
+                setToggleAmountUnit(true);
+                reset();
+              }}
+            >
+              Amount
+            </Button>
+            <Button
+              color={toggleAmountUnit ? "" : "secondary"}
+              onClick={() => {
+                setToggleAmountUnit(false);
+                reset();
+              }}
+            >
+              Units
+            </Button>
+          </ButtonGroup>
+          <div>
+            <TextField
+              type="number"
+              style={{ width: "100%", marginTop: "15px" }}
+              label="Amount"
+              value={amount}
+              variant="filled"
+              disabled={!toggleAmountUnit}
+              onChange={(e) => setAmount(e.target.value)}
+              inputProps={{ step: "1" }}
+            />
+            <TextField
+              type="number"
+              style={{ width: "100%", marginTop: "15px" }}
+              label="Unit"
+              variant="filled"
+              value={unit}
+              onChange={(e) => setUnit(e.target.value)}
+              disabled={toggleAmountUnit}
+              inputProps={{ step: "1" }}
+            />
+          </div>
+        </Grid>
+      </Card>
+    </Container>
   );
 }
 
